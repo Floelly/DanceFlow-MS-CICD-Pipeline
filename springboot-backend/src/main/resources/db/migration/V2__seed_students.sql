ee20807f9cd33a54391285af3e0315d5e1a45b82
Floelly <email@florianbowitz.de>
2026-01-01 14:49:13 +0100

    flyway migration test

---
+++ springboot-backend/src/main/resources/db/migration/V2__seed_students.sql
@@ -0,0 +1,5 @@
+INSERT INTO students (first_name, last_name, email, department_id)
+VALUES
+    ('Anna', 'Muster', 'anna.muster@example.com', 2),
+    ('Ben', 'Tanz', 'ben.tanz@example.com', 1),
+    ('Carla', 'Demo', 'carla.demo@example.com', 2);
