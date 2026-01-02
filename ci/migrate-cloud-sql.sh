0c5903209829e4185ed722956c8c977c46513c39
Floelly <email@florianbowitz.de>
2026-01-02 19:32:22 +0100

    reactivated cloud sql backup and restoration

--- ci/migrate-cloud-sql.sh
+++ ci/migrate-cloud-sql.sh
@@ -11,10 +11,10 @@ DESCRIPTION="jenkins-${PIPELINE_VERSION}-build-${BUILD_NUMBER}"
 
 echo "Backup Cloud SQL instance '${CLOUD_SQL_INSTANCE}' with description '${DESCRIPTION}'..."
 
-#gcloud sql backups create \
-#  --instance="${CLOUD_SQL_INSTANCE}" \
-#  --project="${GCP_PROJECT_ID}" \
-#  --description="${DESCRIPTION}"
+gcloud sql backups create \
+  --instance="${CLOUD_SQL_INSTANCE}" \
+  --project="${GCP_PROJECT_ID}" \
+  --description="${DESCRIPTION}"
 
 echo "Backup for '${CLOUD_SQL_INSTANCE}' successful."
 echo "Start Flyway migration..."
@@ -24,22 +24,22 @@ if bash ci/flyway-migration.sh; then
 else
   echo "Migration not successful, try to restore backup '${DESCRIPTION}'..."
 
-#  BACKUP_ID="$(gcloud sql backups list \
-#    --instance="${CLOUD_SQL_INSTANCE}" \
-#    --filter="description:${DESCRIPTION}" \
-#    --sort-by="~startTime" \
-#    --format="value(id)" | head -n1)"
-#
-#  if [ -z "${BACKUP_ID}" ]; then
-#    echo "No backup with description '${DESCRIPTION}' found. Restoration not successful. Please restore manually!."
-#    exit 1
-#  fi
-#
-#  echo "Restore backup id '${BACKUP_ID}' for instance '${CLOUD_SQL_INSTANCE}'..."
-#  gcloud sql backups restore "${BACKUP_ID}" \
-#    --restore-instance="${CLOUD_SQL_INSTANCE}" \
-#    --project="${GCP_PROJECT_ID}" \
-#    --quiet
+  BACKUP_ID="$(gcloud sql backups list \
+    --instance="${CLOUD_SQL_INSTANCE}" \
+    --filter="description:${DESCRIPTION}" \
+    --sort-by="~startTime" \
+    --format="value(id)" | head -n1)"
+
+  if [ -z "${BACKUP_ID}" ]; then
+    echo "No backup with description '${DESCRIPTION}' found. Restoration not successful. Please restore manually!."
+    exit 1
+  fi
+
+  echo "Restore backup id '${BACKUP_ID}' for instance '${CLOUD_SQL_INSTANCE}'..."
+  gcloud sql backups restore "${BACKUP_ID}" \
+    --restore-instance="${CLOUD_SQL_INSTANCE}" \
+    --project="${GCP_PROJECT_ID}" \
+    --quiet
 
   echo "Restoration successful. Please verify integrity of Cloud SQL instance."
   exit 1
