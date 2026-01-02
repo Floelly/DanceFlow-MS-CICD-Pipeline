34d7f9c01f5a213be6e51f47d4d36bb4747a39f2
Floelly <email@florianbowitz.de>
2026-01-02 15:43:48 +0100

    added restoration functionality to database migration script

--- ci/migrate-cloud-sql.sh
+++ ci/migrate-cloud-sql.sh
@@ -11,10 +11,35 @@ BUILD_NUMBER="${BUILD_NUMBER:-0}"
 
 DESCRIPTION="jenkins-${PIPELINE_VERSION}-build-${BUILD_NUMBER}"
 
-echo "Erstelle Cloud SQL Backup für '${CLOUD_SQL_INSTANCE}' mit Beschreibung '${DESCRIPTION}'..."
+echo "Backup Cloud SQL instance '${CLOUD_SQL_INSTANCE}' with description '${DESCRIPTION}'..."
 
 gcloud sql backups create \
   --instance="${CLOUD_SQL_INSTANCE}" \
   --description="${DESCRIPTION}"
 
-echo "Backup für '${CLOUD_SQL_INSTANCE}' erfolgreich erstellt."
+echo "Backup for '${CLOUD_SQL_INSTANCE}' successful."
+echo "Start Flyway migration..."
+
+if bash ci/flyway-migration.sh; then
+  echo "Migration successful."
+else
+  echo "Migration not successful, try to restore backup '${DESCRIPTION}'..."
+
+  BACKUP_ID="$(gcloud sql backups list \
+    --instance="${CLOUD_SQL_INSTANCE}" \
+    --filter="description:${DESCRIPTION}" \
+    --sort-by="~startTime" \
+    --format="value(id)" | head -n1)"
+
+  if [ -z "${BACKUP_ID}" ]; then
+    echo "No backup with description '${DESCRIPTION}' found. Restoration not successful. Please restore manually!."
+    exit 1
+  fi
+
+  echo "Restore backup id '${BACKUP_ID}' for instance '${CLOUD_SQL_INSTANCE}'..."
+  gcloud sql backups restore "${BACKUP_ID}" \
+    --instance="${CLOUD_SQL_INSTANCE}"
+
+  echo "Restoration startet. Please verify integrity of Cloud SQL instance."
+  exit 1
+fi
