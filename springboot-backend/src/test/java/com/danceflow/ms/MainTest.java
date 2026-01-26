1fa22cde9636ae72afd2f757065371b6c08b9bf5
Floelly <email@florianbowitz.de>
2026-01-26 16:47:33 +0100

    jenkins-pipeline-test: backend test fails!

--- springboot-backend/src/test/java/com/danceflow/ms/MainTest.java
+++ springboot-backend/src/test/java/com/danceflow/ms/MainTest.java
@@ -2,10 +2,13 @@ package com.danceflow.ms;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class MainTest {
 
 	@Test
 	void unitTestRuns() {
+		assertTrue(false);
 	}
 
 }
